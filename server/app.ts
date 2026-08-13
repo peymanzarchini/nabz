7ae2ee6b4adcf2fbee86d39b905e8f846307e80b
p.zarchini <p.zarchini@nitro.local>
2026-08-13 19:33:58 +0330

    fixed bug

--- server/app.ts
+++ server/app.ts
@@ -2,7 +2,7 @@ import { createServer } from "http";
 import { Server as SocketServer } from "socket.io";
 import jwt from "jsonwebtoken";
 import { AuthenticatedJwtPayload } from "./types/index.js";
-import express, { Application } from "express";
+import express, { Express } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
@@ -23,7 +23,7 @@ import { Conversation } from "./modules/marketplace/models/conversation.model.js
 import { Op } from "@sequelize/core";
 import { startAiCronJobs } from "./modules/ai/utils/ai.scheduler.js";
 
-const app: Application = express();
+const app: Express = express();
 
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(
